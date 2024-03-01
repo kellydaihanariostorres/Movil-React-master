af456bd325e2d23a2c319f6cef4b1d043ea7486b
kelly <114171338+kellydaihanariostorres@users.noreply.github.com>
2024-03-01 16:26:04 -0500

    Base de menu

--- rendi/Admi/Index.jsx
+++ rendi/Admi/Index.jsx
@@ -1,9 +1,210 @@
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import React, { useRef, useState } from 'react';
+import { Animated, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import azul from '../ima/azul.png';
+// Tab ICons...
+import home from '../ima/home.png';
 
-function Administrador() {
+// Menu
+import menu from '../ima/menu.png';
+import close from '../ima/menu.png';
+
+
+
+
+export default function App() {
+  const [currentTab, setCurrentTab] = useState("Home");
+  // To get the curretn Status of menu ...
+  const [showMenu, setShowMenu] = useState(false);
+
+  // Animated Properties...
+
+  const offsetValue = useRef(new Animated.Value(0)).current;
+  // Scale Intially must be One...
+  const scaleValue = useRef(new Animated.Value(1)).current;
+  const closeButtonOffset = useRef(new Animated.Value(0)).current;
+
   return (
-    <div>Administrador</div>
-  )
+    <SafeAreaView style={styles.container}>
+
+      <View style={{ justifyContent: 'flex-start', padding: 15 }}>
+        <Image source={azul} style={{
+          width: 60,
+          height: 60,
+          borderRadius: 10,
+          marginTop: 8
+        }}></Image>
+
+        <Text style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: 'white',
+          marginTop: 20
+        }}>Jenna Ezarik</Text>
+
+        <TouchableOpacity>
+          <Text style={{
+            marginTop: 6,
+            color: 'white'
+          }}>View Profile</Text>
+        </TouchableOpacity>
+
+        <View style={{ flexGrow: 1, marginTop: 50 }}>
+          {
+            // Tab Bar Buttons....
+          }
+
+          {TabButton(currentTab, setCurrentTab, "Home", home)}
+          {TabButton(currentTab, setCurrentTab, "Search", home)}
+          {TabButton(currentTab, setCurrentTab, "Notifications", home)}
+          {TabButton(currentTab, setCurrentTab, "Settings", home)}
+
+        </View>
+
+        <View>
+          {TabButton(currentTab, setCurrentTab, "LogOut", home)}
+        </View>
+
+      </View>
+
+      {
+        // Over lay View...
+      }
+
+      <Animated.View style={{
+        flexGrow: 1,
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingHorizontal: 15,
+        paddingVertical: 20,
+        borderRadius: showMenu ? 15 : 0,
+        // Transforming View...
+        transform: [
+          { scale: scaleValue },
+          { translateX: offsetValue }
+        ]
+      }}>
+
+        {
+          // Menu Button...
+        }
+
+        <Animated.View style={{
+          transform: [{
+            translateY: closeButtonOffset
+          }]
+        }}>
+          <TouchableOpacity onPress={() => {
+            // Do Actions Here....
+            // Scaling the view...
+            Animated.timing(scaleValue, {
+              toValue: showMenu ? 1 : 0.88,
+              duration: 300,
+              useNativeDriver: true
+            })
+              .start()
+
+            Animated.timing(offsetValue, {
+              // YOur Random Value...
+              toValue: showMenu ? 0 : 230,
+              duration: 300,
+              useNativeDriver: true
+            })
+              .start()
+
+            Animated.timing(closeButtonOffset, {
+              // YOur Random Value...
+              toValue: !showMenu ? -30 : 0,
+              duration: 300,
+              useNativeDriver: true
+            })
+              .start()
+
+            setShowMenu(!showMenu);
+          }}>
+
+            <Image source={showMenu ? close : menu} style={{
+              width: 20,
+              height: 20,
+              tintColor: 'black',
+              marginTop: 40,
+
+            }}></Image>
+
+          </TouchableOpacity>
+
+          <Text style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            color: 'black',
+            paddingTop: 20
+          }}>{currentTab}</Text>
+
+          <Text style={{
+            fontSize: 20,
+            fontWeight: 'bold'
+            , paddingTop: 15,
+            paddingBottom: 5
+          }}>Jenna Ezarik</Text>
+
+          <Text style={{
+          }}>Techie, YouTuber, PS Lover, Apple Sheep's Sister</Text>
+        </Animated.View>
+
+      </Animated.View>
+
+    </SafeAreaView>
+  );
 }
 
-export default Administrador
+// For multiple Buttons...
+const TabButton = (currentTab, setCurrentTab, title, image) => {
+  return (
+
+    <TouchableOpacity onPress={() => {
+      if (title === "LogOut") {
+        // Lógica para cerrar sesión
+      } else {
+        setCurrentTab(title);
+      }
+    }}>
+      <View style={{
+        flexDirection: "row",
+        alignItems: 'center',
+        paddingVertical: 8,
+        backgroundColor: currentTab === title ? 'white' : 'transparent',
+        paddingLeft: 13,
+        paddingRight: 35,
+        borderRadius: 8,
+        marginTop: 15
+      }}>
+
+        <Image source={image} style={{
+          width: 25, height: 25,
+          tintColor: currentTab === title ? "#5359D1" : "white"
+        }} />
+
+        <Text style={{
+          fontSize: 15,
+          fontWeight: 'bold',
+          paddingLeft: 15,
+          color: currentTab === title ? "#5359D1" : "white"
+        }}>{title}</Text>
+
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5359D1',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+});
